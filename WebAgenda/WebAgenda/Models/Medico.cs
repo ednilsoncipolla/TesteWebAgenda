cee7a4a857c4a0ceed3c919a8b386eab03b9e347
Ednilson Luciano Cipolla <ednilson.cipolla@gmail.com>
2018-02-13 19:46:07 -0200

    Ok

---
+++ WebAgenda/WebAgenda/Models/Medico.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebAgenda.Models
+{
+    public class Medico
+    {
+        #region Atributos
+        int med_Id = 0;
+        string med_Nome;
+        string med_Email;
+        string med_Telefones;
+        string med_Obs;
+
+        public int Med_Id { get => med_Id; set => med_Id = value; }
+        public string Med_Nome { get => med_Nome; set => med_Nome = value; }
+        public string Med_Email { get => med_Email; set => med_Email = value; }
+        public string Med_Telefones { get => med_Telefones; set => med_Telefones = value; }
+        public string Med_Obs { get => med_Obs; set => med_Obs = value; }
+
+        #endregion
+
+        #region CRUD
+        public DataTable GetDtMedicos(string pWhere)
+        {
+            DataTable dt = new DataTable();
+
+            string sSql = $"Select {Environment.NewLine} " +
+                $"  Med_Id {Environment.NewLine} " +
+                $"  , Med_Nome {Environment.NewLine} " +
+                $"  , Med_Email {Environment.NewLine} " +
+                $"  , Med_Telefones {Environment.NewLine} " +
+                $"  , Med_Obs {Environment.NewLine} " +
+                $"from medicos {Environment.NewLine} " +
+                (string.IsNullOrEmpty(pWhere) ? "" : $"where {pWhere} {Environment.NewLine} ");
+
+            dt = BdMySql.getDataTable(sSql);
+
+            return dt;
+        }
+
+        public void Gravar()
+        {
+            string sSql = "";
+            StringBuilder sLog = new StringBuilder();
+
+            if (
+                string.IsNullOrEmpty(Med_Nome)
+                && string.IsNullOrEmpty(Med_Email)
+                && string.IsNullOrEmpty(Med_Telefones)
+                && string.IsNullOrEmpty(Med_Obs)
+               )
+            {
+                throw new System.ArgumentException("Não fora informado dados a serem salvos!", "Salvar informações do Médido");
+            }
+
+            Med_Obs = Med_Obs.Replace("'", "`");
+
+            if (Med_Id > 0)
+            {
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Alterou informações do Médico com ID {Med_Id} ");
+
+                DataTable dtAntes = GetDtMedicos($"Where Usu_Id = {Med_Id}");
+
+                if (dtAntes.Rows.Count == 0)
+                {
+                    throw new System.ArgumentException($"Não foi possivel localizar o Médido com ID {Med_Id}!", "Salvar informações de Médico");
+                }
+
+                StringBuilder sCampos = new StringBuilder();
+
+                if (!string.IsNullOrEmpty(Med_Nome) && dtAntes.Rows[0]["Usu_Nome"].ToString() != Med_Nome.Trim())
+                {
+                    sCampos.AppendLine($", Usu_Nome = '{Med_Nome}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Nome do Médico de {dtAntes.Rows[0]["Usu_Nome"].ToString()} para {Med_Nome}");
+                }
+
+                if (!string.IsNullOrEmpty(Med_Email) && dtAntes.Rows[0]["Usu_Login"].ToString() != Med_Email.Trim())
+                {
+                    sCampos.AppendLine($", Med_Email = '{Med_Email}' {Environment.NewLine} ");
+                    sLog.AppendLine($"e-Mails do Médico de {dtAntes.Rows[0]["Med_Email"].ToString()} para {Med_Email}");
+                }
+
+                if (!string.IsNullOrEmpty(Med_Telefones) && dtAntes.Rows[0]["Usu_Senha"].ToString() != Med_Telefones.Trim())
+                {
+                    sCampos.AppendLine($", Med_Telefones = '{Med_Telefones}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Teledones do Médico de {dtAntes.Rows[0]["Med_Telefones"].ToString()} para {Med_Telefones}");
+                }
+
+                if (!string.IsNullOrEmpty(Med_Obs) && dtAntes.Rows[0]["Med_Obs"].ToString() != Med_Obs.Trim())
+                {
+                    sCampos.AppendLine($", Med_Obs = '{Med_Obs}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Observações do Médico de [{dtAntes.Rows[0]["Med_Obs"].ToString()}] para [{Med_Obs}] ");
+                }
+
+                if (string.IsNullOrEmpty(sCampos.ToString()))
+                {
+                    throw new System.ArgumentException($"Não foi possivel localizada alterações a serem salvas!", "Salvar informações do Médico");
+                }
+
+                sSql = $"Update medicos set {Environment.NewLine} " +
+                    sCampos.ToString().Substring(2) +
+                    $"Where Med_Id = {Med_Id} { Environment.NewLine} ";
+
+                BdMySql.executaComando(sSql);
+                Logs.Incluir(sLog.ToString());
+            }
+            else
+            {
+                if (
+                    string.IsNullOrEmpty(Med_Nome)
+                    || string.IsNullOrEmpty(Med_Email)
+                    || string.IsNullOrEmpty(Med_Telefones)
+                   )
+                {
+                    throw new System.ArgumentException("É obrigatório informar Nome, Email e Telefones!", "Incluir informações do Médico");
+                }
+
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Incluiu informações do Usuário");
+                sLog.AppendLine($"Nome do Médico => {Med_Nome}");
+                sLog.AppendLine($"e-Mail do Médico => {Med_Email}");
+                sLog.AppendLine($"Teledones do Médico => {Med_Telefones}");
+                sLog.AppendLine($"Observações para o Médico => { Med_Obs }");
+
+                sSql = $"Insert into Usuarios ( {Environment.NewLine} " +
+                    $"    Med_Id {Environment.NewLine} " +
+                    $"    , Med_Nome {Environment.NewLine} " +
+                    $"    , Med_Email {Environment.NewLine} " +
+                    $"    , Med_Telefones {Environment.NewLine} " +
+                    $"    , Med_Obs {Environment.NewLine} " +
+                    $") values( {Environment.NewLine} " +
+                    $"    {Med_Id} {Environment.NewLine} " +
+                    $"    , '{Med_Nome}' {Environment.NewLine} " +
+                    $"    , '{Med_Email}' {Environment.NewLine} " +
+                    $"    , '{Med_Telefones}' {Environment.NewLine} " +
+                    $"    , '{Med_Obs}' {Environment.NewLine} " +
+                    $") {Environment.NewLine} ";
+
+                Med_Id = BdMySql.executaInsertRecuperaId(sSql);
+
+            }
+        }
+
+        public void GetUsuario()
+        {
+            if (Med_Id <= 0)
+            {
+                throw new System.ArgumentException("Obrigatório informar o ID Médido", "Carregar Médido");
+            }
+            else
+            {
+                string sWhere = $" Where Med_Id = {Med_Id} {Environment.NewLine} ";
+
+                DataTable dt = GetDtMedicos(sWhere);
+
+                if (dt.Rows.Count > 0)
+                {
+                    Med_Id = Convert.ToInt32(dt.Rows[0]["Med_Id"]);
+                    Med_Nome = dt.Rows[0]["Med_Nome"].ToString();
+                    Med_Email = dt.Rows[0]["Med_Email"].ToString();
+                    Med_Obs = dt.Rows[0]["Med_Obs"].ToString();
+                }
+                else
+                {
+                    throw new System.ArgumentException("Não foi possivel localizar o Médico com as informações fornecidas!", "Carregar Médico");
+                }
+            }
+        }
+
+        public void Excluir()
+        {
+            if (Med_Id <= 0)
+            {
+                throw new System.ArgumentException("Obrigatório informar o ID ", "Excluir Médico");
+            }
+            else
+            {
+                GetUsuario();
+                StringBuilder sLog = new StringBuilder();
+
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Incluiu informações do Médico");
+                sLog.AppendLine($"Nome do Médico => {Med_Nome}");
+                sLog.AppendLine($"e-Mail do Médico => {Med_Email}");
+                sLog.AppendLine($"Telefones do Médico => {Med_Telefones}");
+                sLog.AppendLine($"Obs. do Médico => {Med_Obs}");
+
+
+                string sSql = $"Delete from medico {Environment.NewLine} " +
+                    $"where Med_Id = {Med_Id} ";
+
+                int QtdeReg = BdMySql.executaComando(sSql);
+
+                if (QtdeReg == 0)
+                { throw new System.ArgumentException($"Não foi possível localizar o Médico com ID {Med_Id}", "Excluir Médico"); }
+                else
+                { Logs.Incluir(sLog.ToString()); }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ WebAgenda/WebAgenda/Models/Paciente.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebAgenda.Models
+{
+    public class Paciente
+    {
+        #region Atributos
+        int pac_Id = 0;
+        string pac_Nome;
+        string pac_Email;
+        string pac_Telefones;
+        string pac_Obs;
+
+        public int Pac_Id { get => pac_Id; set => pac_Id = value; }
+        public string Pac_Nome { get => pac_Nome; set => pac_Nome = value; }
+        public string Pac_Email { get => pac_Email; set => pac_Email = value; }
+        public string Pac_Telefones { get => pac_Telefones; set => pac_Telefones = value; }
+        public string Pac_Obs { get => pac_Obs; set => pac_Obs = value; }
+
+        #endregion
+
+        #region CRUD
+        public DataTable GetDtPacientes(string pWhere)
+        {
+            DataTable dt = new DataTable();
+
+            string sSql = $"Select {Environment.NewLine} " +
+                $"  Pac_Id {Environment.NewLine} " +
+                $"  , Pac_Nome {Environment.NewLine} " +
+                $"  , Pac_Email {Environment.NewLine} " +
+                $"  , Pac_Telefones {Environment.NewLine} " +
+                $"  , Pac_Obs {Environment.NewLine} " +
+                $"from Pacientes {Environment.NewLine} " +
+                (string.IsNullOrEmpty(pWhere) ? "" : $"where {pWhere} {Environment.NewLine} ");
+
+            dt = BdMySql.getDataTable(sSql);
+
+            return dt;
+        }
+
+        public void Gravar()
+        {
+            string sSql = "";
+            StringBuilder sLog = new StringBuilder();
+
+            if (
+                string.IsNullOrEmpty(Pac_Nome)
+                && string.IsNullOrEmpty(Pac_Email)
+                && string.IsNullOrEmpty(Pac_Telefones)
+                && string.IsNullOrEmpty(Pac_Obs)
+               )
+            {
+                throw new System.ArgumentException("Não fora informado dados a serem salvos!", "Salvar informações do Passiente");
+            }
+
+            Pac_Obs = Pac_Obs.Replace("'", "`");
+
+            if (Pac_Id > 0)
+            {
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Alterou informações do Passiente com ID {Pac_Id} ");
+
+                DataTable dtAntes = GetDtPacientes($"Where Pass_Id = {Pac_Id}");
+
+                if (dtAntes.Rows.Count == 0)
+                {
+                    throw new System.ArgumentException($"Não foi possivel localizar o Paciente com ID {Pac_Id}!", "Salvar informações de Paciente");
+                }
+
+                StringBuilder sCampos = new StringBuilder();
+
+                if (!string.IsNullOrEmpty(Pac_Nome) && dtAntes.Rows[0]["Pac_Nome"].ToString() != Pac_Nome.Trim())
+                {
+                    sCampos.AppendLine($", Pac_Nome = '{Pac_Nome}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Nome do Paciente de {dtAntes.Rows[0]["Pac_Nome"].ToString()} para {Pac_Nome}");
+                }
+
+                if (!string.IsNullOrEmpty(Pac_Email) && dtAntes.Rows[0]["Pac_Email"].ToString() != Pac_Email.Trim())
+                {
+                    sCampos.AppendLine($", Pac_Email = '{Pac_Email}' {Environment.NewLine} ");
+                    sLog.AppendLine($"e-Mails do Paciente de {dtAntes.Rows[0]["Pac_Email"].ToString()} para {Pac_Email}");
+                }
+
+                if (!string.IsNullOrEmpty(Pac_Telefones) && dtAntes.Rows[0]["Pac_Telefones"].ToString() != Pac_Telefones.Trim())
+                {
+                    sCampos.AppendLine($", Pac_Telefones = '{Pac_Telefones}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Teledones do Paciente de {dtAntes.Rows[0]["Pac_Telefones"].ToString()} para {Pac_Telefones}");
+                }
+
+                if (!string.IsNullOrEmpty(Pac_Obs) && dtAntes.Rows[0]["Pac_Obs"].ToString() != Pac_Obs.Trim())
+                {
+                    sCampos.AppendLine($", Pac_Obs = '{Pac_Obs}' {Environment.NewLine} ");
+                    sLog.AppendLine($"Observações do Paciente de [{dtAntes.Rows[0]["Pac_Obs"].ToString()}] para [{Pac_Obs}] ");
+                }
+
+                if (string.IsNullOrEmpty(sCampos.ToString()))
+                {
+                    throw new System.ArgumentException($"Não foi possivel localizada alterações a serem salvas!", "Salvar informações do Paciente");
+                }
+
+                sSql = $"Update Pacientes set {Environment.NewLine} " +
+                    sCampos.ToString().Substring(2) +
+                    $"Where Pac_Id = {Pac_Id} { Environment.NewLine} ";
+
+                BdMySql.executaComando(sSql);
+                Logs.Incluir(sLog.ToString());
+            }
+            else
+            {
+                if (
+                    string.IsNullOrEmpty(Pac_Nome)
+                    || string.IsNullOrEmpty(Pac_Email)
+                    || string.IsNullOrEmpty(Pac_Telefones)
+                   )
+                {
+                    throw new System.ArgumentException("É obrigatório informar Nome, Email e Telefones do paciente!", "Incluir informações do Paciente");
+                }
+
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Incluiu informações do Paciente");
+                sLog.AppendLine($"Nome do Paciente => {Pac_Nome}");
+                sLog.AppendLine($"e-Mail do Paciente => {Pac_Email}");
+                sLog.AppendLine($"Teledones do Paciente => {Pac_Telefones}");
+                sLog.AppendLine($"Observações para o Paciente => { Pac_Obs }");
+
+                sSql = $"Insert into Pacientes ( {Environment.NewLine} " +
+                    $"    Pac_Id {Environment.NewLine} " +
+                    $"    , Pac_Nome {Environment.NewLine} " +
+                    $"    , Pac_Email {Environment.NewLine} " +
+                    $"    , Pac_Telefones {Environment.NewLine} " +
+                    $"    , Pac_Obs {Environment.NewLine} " +
+                    $") values( {Environment.NewLine} " +
+                    $"    {Pac_Id} {Environment.NewLine} " +
+                    $"    , '{Pac_Nome}' {Environment.NewLine} " +
+                    $"    , '{Pac_Email}' {Environment.NewLine} " +
+                    $"    , '{Pac_Telefones}' {Environment.NewLine} " +
+                    $"    , '{Pac_Obs}' {Environment.NewLine} " +
+                    $") {Environment.NewLine} ";
+
+                Pac_Id = BdMySql.executaInsertRecuperaId(sSql);
+
+            }
+        }
+
+        public void GetPaciente()
+        {
+            if (Pac_Id <= 0)
+            {
+                throw new System.ArgumentException("Obrigatório informar o ID Paciente");
+            }
+            else
+            {
+                string sWhere = $" Where Pac_Id = {Pac_Id} {Environment.NewLine} ";
+
+                DataTable dt = GetDtPacientes(sWhere);
+
+                if (dt.Rows.Count > 0)
+                {
+                    Pac_Id = Convert.ToInt32(dt.Rows[0]["Pac_Id"]);
+                    Pac_Nome = dt.Rows[0]["Pac_Nome"].ToString();
+                    Pac_Email = dt.Rows[0]["Pac_Email"].ToString();
+                    Pac_Obs = dt.Rows[0]["Pac_Obs"].ToString();
+                }
+                else
+                {
+                    throw new System.ArgumentException("Não foi possivel localizar o Médico com as informações fornecidas!");
+                }
+            }
+        }
+
+        public void Excluir()
+        {
+            if (Pac_Id <= 0)
+            {
+                throw new System.ArgumentException("Obrigatório informar o ID ");
+            }
+            else
+            {
+                GetPaciente();
+                StringBuilder sLog = new StringBuilder();
+
+                sLog.AppendLine($"{Sistema.Usuario.Usu_Nome} Incluiu informações do Médico");
+                sLog.AppendLine($"Nome do Paciente => {Pac_Nome}");
+                sLog.AppendLine($"e-Mail do Paciente => {Pac_Email}");
+                sLog.AppendLine($"Telefones do Paciente => {Pac_Telefones}");
+                sLog.AppendLine($"Obs. do Paciente => {Pac_Obs}");
+
+
+                string sSql = $"Delete from pacientes {Environment.NewLine} " +
+                    $"where Pac_Id = {Pac_Id} ";
+
+                int QtdeReg = BdMySql.executaComando(sSql);
+
+                if (QtdeReg == 0)
+                { throw new System.ArgumentException($"Não foi possível localizar o Paciente com ID {Pac_Id}"); }
+                else
+                { Logs.Incluir(sLog.ToString()); }
+            }
+        }
+
+        #endregion
+    }
+}
